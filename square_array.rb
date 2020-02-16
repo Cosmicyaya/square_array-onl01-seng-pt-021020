2e45c9f6c268630825f82c2d08341c62ecdcefec
Ryan Hollins <ballbaroi@gmail.com>
2020-02-16 19:00:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,4 @@
-def square_array(array)
-  
-  square_array = ["1","2","3"]
-  square_array.each do |number|
-    puts number + 1
-  end
+some_array = [1,2,3]
+some_array.each do |number|
+  puts number + 1
+end
